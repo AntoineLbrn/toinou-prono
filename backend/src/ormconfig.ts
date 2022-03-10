3d9f9c94e2a44c1bf97db3e775fa7b15a3007d4d
AntoineLbrn <antoinelmh@hotmail.fr>
2022-03-10 17:42:52 +0100

    [heroku] add connection to heroku db

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -1,5 +1,6 @@
 import { join } from 'path'
 import { ConnectionOptions } from 'typeorm'
+import { parse } from 'url'
 
 const PROD_ENV = 'production'
 
@@ -10,7 +11,26 @@ const config = {
   database: process.env.SQL_DATABASE,
 }
 
-const connectionOptions: ConnectionOptions = {
+const url = process.env.DATABASE_URL;
+
+const connectionOptions: ConnectionOptions = url ? {
+  type: 'postgres',
+  url,
+  synchronize: false,
+  dropSchema: false,
+  // Run migrations automatically,
+  // you can disable this if you prefer running migration manually.
+  migrationsRun: true,
+  logging: ['warn', 'error'],
+  logger: process.env.NODE_ENV === PROD_ENV ? 'file' : 'debug',
+  entities: ['src/app/entities/*.{ts,js}'],
+  migrations: ['src/database/migrations/*.{ts,js}'],
+  cli: {
+    entitiesDir: 'src/app/entities',
+    migrationsDir: 'src/database/migrations'
+  }
+} :  
+{
   type: 'postgres',
   host: 'host.docker.internal',
   port: 5432,
