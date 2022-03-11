6b58ed1ce34fc369f71c9ccede8ff693f02ddd1b
AntoineLbrn <antoinelmh@hotmail.fr>
2022-03-11 09:20:44 +0100

    [heroku] orm config target build folder

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -48,11 +48,11 @@ const connectionOptions: ConnectionOptions = url ? {
   migrationsRun: true,
   logging: ['warn', 'error'],
   logger: process.env.NODE_ENV === PROD_ENV ? 'file' : 'debug',
-  entities: ['src/app/entities/*.{ts,js}'],
-  migrations: ['src/database/migrations/*.{ts,js}'],
+  entities: ['dist/app/entities/*.{ts,js}'],
+  migrations: ['dist/database/migrations/*.{ts,js}'],
   cli: {
-    entitiesDir: 'src/app/entities',
-    migrationsDir: 'src/database/migrations'
+    entitiesDir: 'dist/app/entities',
+    migrationsDir: 'dist/database/migrations'
   }
 }
 
