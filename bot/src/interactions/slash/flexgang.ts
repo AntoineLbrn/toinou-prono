91bf7e2dde1ca5dba6ccfcd57b7cab221fb439af
AntoineLbrn <antoinelmh@hotmail.fr>
2022-04-01 10:45:43 +0200

    [bot-feat] check voice channel for flexgang players

--- bot/src/interactions/slash/flexgang.ts
+++ bot/src/interactions/slash/flexgang.ts
@@ -10,18 +10,26 @@ const LANES = [
 abstract class Flexgang {
   @Slash("flexgang", {description: "Assigne une lane aléatoire à chacun des membres de ton vocal"})
   public flexgang(@SlashOption("joueurs", { description: "Noms des joueurs séparés par des espaces", required: false}) playersAsString: string, interaction: CommandInteraction): void {
+    let players: string[] = [];
     if (!!playersAsString) {
-        const players = playersAsString.split(' '); 
-        const lanes: string[] = JSON.parse(JSON.stringify(LANES));
-        const roleDistribution = new Map<string,string>([]);
-        let playerIndex = 0;
-        for(let i = lanes.length-1;i>=0;i--){
-            roleDistribution.set(lanes.splice(Math.floor(Math.random()*lanes.length), 1)[0], players[playerIndex]);
-            playerIndex++;
-        } 
-        interaction.reply({embeds: [new FlexGangEmbed(LANES, roleDistribution)]});
+        players.push(...playersAsString.split(' ')); 
+    }
+    const voiceChannel = interaction.guild?.members.cache.get(interaction.user.id)?.voice;
+    voiceChannel?.channel?.members.forEach((member) => {
+      players.push(member.user.username);
+    })
+
+    if (!players.length) {
+      interaction.reply("Rejoins un vocal ou définit des joueurs avant d'utiliser cette commande");
     } else {
-        interaction.reply('????');        
+      const lanes: string[] = JSON.parse(JSON.stringify(LANES));
+      const roleDistribution = new Map<string,string>([]);
+      let playerIndex = 0;
+      for(let i = lanes.length-1;i>=0;i--){
+          roleDistribution.set(lanes.splice(Math.floor(Math.random()*lanes.length), 1)[0], players[playerIndex]);
+          playerIndex++;
+      } 
+      interaction.reply({embeds: [new FlexGangEmbed(LANES, roleDistribution)]});  
     }
   }
 }
