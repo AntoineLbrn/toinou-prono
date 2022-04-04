9e8f74df8112c227bbbc8e1f4e41be87ccbd9785
antoine_lbrn <antoinelmh@hotmail.fr>
2022-04-04 11:06:08 +0200

    [fix-bot] do not crash if no tournament list is empty

--- bot/src/useCases/tournaments/SendTournamentsEmbedMessage.ts
+++ bot/src/useCases/tournaments/SendTournamentsEmbedMessage.ts
@@ -5,7 +5,8 @@ import Tournament from "../../models/Tournament";
 class SendTournamentsEmbedMessage {
     public async execute (args: {tournaments: Tournament[], channel: any}): Promise<Message[]> {
         const embeds = args.tournaments.map((tournament) => new TournamentEmbed(tournament));
-        return await args.channel.send({embeds});
+        const content = args.tournaments.length ? '' : "Tu n'es enregistré à aucun tournoi";
+        return await args.channel.send({content, embeds});
     }
 }
 
