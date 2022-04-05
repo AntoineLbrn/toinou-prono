595bf64278dc0f5ef24edef64b6e64e38f4c370c
antoine_lbrn <antoinelmh@hotmail.fr>
2022-04-05 17:09:46 +0200

    [fix-bot] empty message error

--- bot/src/useCases/tournaments/SendTournamentsEmbedMessage.ts
+++ bot/src/useCases/tournaments/SendTournamentsEmbedMessage.ts
@@ -5,7 +5,7 @@ import Tournament from "../../models/Tournament";
 class SendTournamentsEmbedMessage {
     public async execute (args: {tournaments: Tournament[], channel: any}): Promise<Message[]> {
         const embeds = args.tournaments.map((tournament) => new TournamentEmbed(tournament));
-        const content = args.tournaments.length ? '' : "Tu n'es enregistré à aucun tournoi";
+        const content = embeds.length ? undefined : "Tu n'es enregistré à aucun tournoi";
         return await args.channel.send({content, embeds});
     }
 }
