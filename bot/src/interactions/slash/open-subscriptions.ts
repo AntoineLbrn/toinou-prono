e9652de5e2e2b8e2d7e6c926f7eaa08019753c89
antoine_lbrn <antoinelmh@hotmail.fr>
2022-04-12 18:52:29 +0200

    [bot-feat] print selected user name on rank interaction

--- bot/src/interactions/slash/open-subscriptions.ts
+++ bot/src/interactions/slash/open-subscriptions.ts
@@ -3,13 +3,14 @@ import { Discord, Guard, Slash, SlashOption } from "discordx";
 import isAdmin from "../../decorators/isAdmin";
 import getSubscriptionByTournamentLabelAndServerId from "../../api/subscriptions/getSubscriptionByTournamentLabelAndServerId";
 import SendParticipationButton from "../../useCases/participations/SendParticipationButton";
+import { autocompleteTournaments } from "../../utils/autocompleteTournaments";
 
 @Discord()
 abstract class OpenParticipation {
 
   @Slash("open-participation", {description: "Permet aux membres du serveur de s'inscrire à une compétition"})
   @Guard(isAdmin)
-  public openSubscription(@SlashOption("tournament", { description: "Nom du tournoi à configurer", required: true}) tournamentName: string, interaction: CommandInteraction): void {
+  public openSubscription(@SlashOption("tournament", { description: "Nom du tournoi à configurer", autocomplete: autocompleteTournaments, type: "STRING", required: true}) tournamentName: string, interaction: CommandInteraction): void {
     if (interaction.guildId) {
         getSubscriptionByTournamentLabelAndServerId(tournamentName, interaction.guildId).then((subscription) => {
             if (subscription) {
--- bot/src/interactions/slash/rank.ts
+++ bot/src/interactions/slash/rank.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, User } from "discord.js";
+import { CommandInteraction, GuildMember, User } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 import { autocompleteTournaments } from "../../utils/autocompleteTournaments";
 import getRankingByUserDiscordIdAndTournamentLabel from "../../api/subscriptions/getRankingByUserDiscordIdAndTournamentLabel";
@@ -8,11 +8,11 @@ abstract class Rank {
     @Slash("rank", {description: 'Affiche ton rank pour une compétition'})
     public rank(
         @SlashOption("tournament", { description: "Nom du tournoi à consulter", autocomplete: autocompleteTournaments, type: "STRING", required: true}) tournamentName: string,
-        @SlashOption("user", { description: "Utilisateur à consulter", type: "USER", required: false}) user: User,
+        @SlashOption("user", { description: "Utilisateur à consulter", type: "USER", required: false}) member: GuildMember,
         interaction: CommandInteraction,
     ): void {
-        getRankingByUserDiscordIdAndTournamentLabel({discordUserId: user ? user.id : interaction.user.id, label: tournamentName}).then((ranking) => {
-            interaction.reply(`Tu es classé ${ranking.rank} : ${ranking.points} points`);
+        getRankingByUserDiscordIdAndTournamentLabel({discordUserId: member ? member.id : interaction.user.id, label: tournamentName}).then((ranking) => {
+            interaction.reply(`${member ? member.user.username + ' est' : 'Tu es'} classé ${ranking.rank} : ${ranking.points} points`);
         }).catch((error) => {
             interaction.reply(error.message);
         });
